48d732406edf6ad396fdaa974200bf8417f6b861
anjaroki <anjar.oki@gmail.com>
2022-07-23 22:35:18 +0700

    create home page, tapi masih 90% karena ga kebaca file logo dari folder assest yang ada di public

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Signup from "./Signup";
+import Home from "./Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Signup />
+    <Home />
   </React.StrictMode>
 );
 
