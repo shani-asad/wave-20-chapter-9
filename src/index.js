f8438eae4c2afb784a05ae68e52cabd94d126d75
anjaroki <anjar.oki@gmail.com>
2022-07-23 22:27:07 +0700

    create login page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Home";
+import Login from "./Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Login />
   </React.StrictMode>
 );
 
